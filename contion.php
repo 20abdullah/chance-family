dcd1edcdc84f485d7cc719e35135e0b994b9c4eb
20abdullah <113922299+20abdullah@users.noreply.github.com>
2022-10-10 01:15:51 +0200

    Add files via upload

---
+++ contion.php
@@ -0,0 +1,8 @@
+<?php 
+$server ="localhost";
+$username ="id19679408_abdullah";
+$password ="9Yb)y{TU5Ww={muw";
+$dbname ="id19679408_data";
+
+$conn =mysqli_connect($server,$username,$password,$dbname);
+?>
